b4e50b6fc9528532dd1bf03c74fa24d81bd4da53
AA <ahmed3del2003@gmail.com>
2024-02-02 17:44:16 +0100

    2/2

--- app.js
+++ app.js
@@ -5,6 +5,37 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'assets')));
+app.use('/pages', express.static(path.join(__dirname, 'pages')));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://mariem:WecrMJfHDGZ7Jh1c@cluster2.8cbx6bk.mongodb.net/ISIMM_Test")
+
+const testSchema = {
+  email: String,
+  query: String,
+}; 
+
+const test_model = mongoose.model("test", testSchema);
+
+app.post("/pages/institut", function (req, res) {
+
+  const data = new test_model({
+     email: req.body.email,
+     query: req.body.query,
+  });
+
+  data.save()
+     .then(() => res.send('Data saved successfully'))
+     .catch(err => res.status(500).send('Error saving data: ' + err));
+});
+
+
+
 
 
 // Define a route to render the HTML page
